4a9a0b0c9e56b0a372706497d019916d3c26de9d
hyderaliafzal <hyder7766@gmail.com>
2025-08-20 19:15:39 +0500

    remove the extras fields from user entity..

--- Entities/Connekta/User.cs
+++ Entities/Connekta/User.cs
@@ -2,16 +2,8 @@
 public class User
 {
     [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long ID { get; set; }
-    public long CreatedById { get; set; }
-    public long UpdatedById { get; set; }
-    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
-    public DateTime UpdatedDate { get; set; } = DateTime.UtcNow;
     public bool IsDeleted { get; set; } = false;
-    [NotMapped]
-    public string TenantType { get; set; }
-
     public string PhotoURL { get; set; }
     [Required]
     public string FirstName { get; set; }
@@ -19,14 +11,6 @@ public class User
     public string LastName { get; set; }
     [Required]
     public string Email { get; set; }
-    public string Password { get; set; }
-    public string Designation { get; set; }
-    public string Domain { get; set; }
-    public UserStatus UserStatus { get; set; }
-    public string Role { get; set; }
-    public string PhoneCode { get; set; }
-    public string PhoneNumber { get; set; }
-    public string CompanyPhone { get; set; }
     public DateTime? LoginSession { get; set; }
 
     [ForeignKey("PermissionGroup")]
